10d7a6b530ad00590055a873d4a496569e3be155
ilyes-chabab <ilyes.chabab@laplateforme.io>
2024-09-11 12:52:24 +0200

    formation de texte

--- maze_solver.py
+++ maze_solver.py
@@ -1,57 +1,59 @@
 import sys
 
-labyrinthe = [
-    ['.', '.', '#', '#', '#', '#', '#'],
-    ['#', '.', '#', '.', '.', '.', '#'],
-    ['#', '.', '.', '.', '#', '.', '#'],
-    ['#', '.', '.', '.', '.', '.', '#'],
-    ['#', '.', '#', '#', '#', '#', '#'],
-    ['#', '.', '.', '.', '.', '.', '#'],
-    ['#', '#', '#', '#', '.', '.', '.']
+maze = [
+    [".", ".", "#", "#", "#", "#", "#"],
+    ["#", ".", "#", ".", ".", ".", "#"],
+    ["#", ".", ".", ".", "#", ".", "#"],
+    ["#", ".", ".", ".", ".", ".", "#"],
+    ["#", ".", "#", "#", "#", "#", "#"],
+    ["#", ".", ".", ".", ".", ".", "#"],
+    ["#", "#", "#", "#", ".", ".", "."],
 ]
 
-file_name= input("Quel nom de fichier voulez-vous ? : ")
-start = (0,0)
-size_of_labyrinthe = 7
-end = (size_of_labyrinthe -1,size_of_labyrinthe -1)
-    
-def MazeSolver(labyrinthe,x,y):
-    
-    if x < 0 or x > len(labyrinthe) or y < 0 or y > len(labyrinthe):
-        return False
-    
-    if labyrinthe[x][y] == '#' or labyrinthe[x][y] == '*' or labyrinthe[x][y] == 'o':
-        return False
-    
-    if x == len(labyrinthe) - 1 and y == len(labyrinthe) - 1:
-        labyrinthe[x][y] = 'o'
-        return True
-    
-    labyrinthe[x][y] = 'o'
+file_name = input("Quel nom de fichier voulez-vous ? : ")
+start = (0, 0)
+size_of_maze = 7
+end = (size_of_maze - 1, size_of_maze - 1)
 
-    if (MazeSolver(labyrinthe, x, y + 1) or  
-        MazeSolver(labyrinthe, x + 1, y) or  
-        MazeSolver(labyrinthe, x, y - 1) or  
-        MazeSolver(labyrinthe, x - 1, y)):   
+
+def MazeSolver(maze, x, y):
+
+    if x < 0 or x > len(maze) or y < 0 or y > len(maze):
+        return False
+
+    if maze[x][y] == "#" or maze[x][y] == "*" or maze[x][y] == "o":
+        return False
+
+    if x == len(maze) - 1 and y == len(maze) - 1:
+        maze[x][y] = "o"
         return True
 
-    labyrinthe[x][y] = '*'
+    maze[x][y] = "o"
+
+    if (
+        MazeSolver(maze, x, y + 1)
+        or MazeSolver(maze, x + 1, y)
+        or MazeSolver(maze, x, y - 1)
+        or MazeSolver(maze, x - 1, y)
+    ):
+        return True
+
+    maze[x][y] = "*"
     return False
 
-    
 
-def PrintLabyrinthe(labyrinthe):
-    for case in labyrinthe:
+def Printmaze(maze):
+    for case in maze:
         print("".join(case))
- 
-
-MazeSolver(labyrinthe,0,0) 
-PrintLabyrinthe(labyrinthe) 
 
 
-with open(f"{file_name}.txt","w") as file:
+MazeSolver(maze, 0, 0)
+Printmaze(maze)
+
+
+with open(f"{file_name}.txt", "w") as file:
     sys.stdout = file
-    
-    PrintLabyrinthe(labyrinthe) 
+
+    Printmaze(maze)
 
     sys.stdout = sys.__stdout__
