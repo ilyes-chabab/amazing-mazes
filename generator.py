2455303f8b6c67df6aca62cb379df1a4d727328c
Ayoub Abderrahmane <ayoub-abderrahmane@laplateforme.io>
2024-09-12 18:09:45 +0200

    starting recursing backtrack function

--- generator.py
+++ generator.py
@@ -1,3 +1,5 @@
+import random
+
 def maze_size():
     size = input("How big does the maze need to be ? (one number only) : ")
     size = int(size)
@@ -11,18 +13,24 @@ def choose_file_name():
 
 def create_maze():
     l = []
-    a = 2 * maze_size() + 1
-    for i in range(a):
-        row = "#" * a
+    new_size = 2 * maze_size() + 1
+    for i in range(new_size):
+        row = ["#"] * new_size
         l.append(row)
     return l
 
 
-def create_maze_file():
-    maze = create_maze()
+def create_maze_file(maze):
     with open(choose_file_name(), "w") as file:
         for line in maze:
-            file.write(line + "\n")
-        
+            file.write("".join(line) + "\n")
 
-create_maze_file()
+
+def main():
+    maze = create_maze()
+    create_maze_file(maze)
+    
+
+
+if __name__ == "__main__":
+    main()
