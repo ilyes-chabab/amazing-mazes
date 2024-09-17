3def8f8a05433a349d05900ecb03cb7f78334f8e
Ayoub Abderrahmane <ayoub-abderrahmane@laplateforme.io>
2024-09-17 10:21:55 +0200

    update the error that shows twice the input_maze_size()

--- generator.py
+++ generator.py
@@ -2,12 +2,10 @@ import random
 
 def input_maze_size():
     size = input("How big does the maze need to be ? (one number only) : ")
-    # size = int(size) # Transform the str in a int to return it properly
     return int(size)
 
 def real_maze_size(size):
-    return 2 * size + 1 
-    # return size # returning the "real size" of the maze to generate between the good random.randint
+    return 2 * size + 1
 
 
 def choose_file_name():
@@ -15,9 +13,9 @@ def choose_file_name():
     return file # Return the file name choosen by the user
 
 
-def create_maze():
+def create_maze(size):
     l = []
-    new_size = 2 * maze_size() + 1
+    new_size = real_maze_size(size)
     for i in range(new_size):
         row = ["#"] * new_size # Creating our lines for the maze
         l.append(row) # adding the lines to our maze
@@ -29,15 +27,15 @@ def create_maze_file(maze):
         for line in maze:
             file.write("".join(line) + "\n") # Writing the maze in a file without [] or ""
 
-def generate_random_position(size):
-    # random_numbers = []
+def generate_random_positions(size):
+    random_positions = []
     real_size = real_maze_size(size)
-    # for i in range(2):
-    #     i = random.randint(1,size)
-    #     random_numbers.append(i)
-    # return random_numbers
+    for i in range(2):
+        i = random.randint(1,real_size - 2)
+        random_positions.append(i)
+    return random_positions
 
-    return [random.randint(1,real_size), random.randint(1,real_size)]
+    # return [random.randint(1,real_size), random.randint(1,real_size)]
 
 def generate_random_directions():
     d = random.randint(1,4)
@@ -45,23 +43,26 @@ def generate_random_directions():
 
 def recursive_backtrack():
     direction = generate_random_directions()
-    numbers = generate_random_numbers()
-    maze = create_maze()
+    numbers = generate_random_positions(size)
+    maze = create_maze(size)
     if direction == 1:
         maze[numbers[0]][numbers[1]] = maze[numbers[0]][numbers[1] + 1]
+        print ("up")
     elif direction == 2:
         maze[numbers[0]][numbers[1]] = maze[numbers[0] + 1][numbers[1]]
+        print ("right")
     elif direction == 3:
         maze[numbers[0]][numbers[1]] = maze[numbers[0]][numbers[1] - 1]
+        print ("down")
     elif direction == 4:
         maze[numbers[0]][numbers[1]] = maze[numbers[0] - 1][numbers[1]]
+        print ("left")
+
 
 def main():
-    n = input_maze_size()
-    # maze = create_maze(n)
-    # create_maze_file(maze)
-    recursive_backtrack()
-
+    size = input_maze_size()
+    recursive_backtrack(size)
+    
 
 if __name__ == "__main__":
     main()
