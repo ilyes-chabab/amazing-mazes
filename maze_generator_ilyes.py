1db274f9b74982d67d6b959987e162b21ceec04a
ilyes-chabab <ilyes.chabab@laplateforme.io>
2024-09-20 12:36:22 +0200

    version finale du generator et du solver en backtracking

--- maze_generator_ilyes.py
+++ maze_generator_ilyes.py
@@ -1,65 +1,89 @@
 import random
+import sys
 
-# size = int(input("Quelle taille de labyrinthe voullez-vous ? : "))
-# if size % 2 !=1:
-#     size +=1    
-size = 3
-size = 2* size +1
-maze=[]
-generator_movement = [(-2, 0), (2, 0), (0, -2), (0, 2)]
-solver_movement = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-generator_stack=[]
-solver_stack=[]
+size = int(input("Quelle taille de labyrinthe voullez-vous ? : "))
+name_file = str(input("Quel nom de fichier voullez-vous ? : "))
 
-def BaseOfTheMaze(size , maze):
+if size % 2 != 1:
+    size += 1
+
+size = 2 * size + 1
+maze = []
+MOVES = [(-2, 0), (2, 0), (0, -2), (0, 2)]
+generator_stack = []
+
+
+def BaseOfTheMaze(size, maze):
     maze = [["#" for i in range(size)] for i in range(size)]
 
     maze[0][0] = "."
     maze[1][0] = "."
-    maze[size-2][size-1] = "."
-    maze[size-1][size-1] = "."
+    maze[size - 2][size - 1] = "."
+    maze[size - 1][size - 1] = "."
 
-    for i in range(1,size-1):
-        for j in range (size):
-            if i % 2 != 0 and j % 2!=0 :
+    for i in range(1, size - 1):
+        for j in range(size):
+            if i % 2 != 0 and j % 2 != 0:
                 maze[i][j] = "."
-    
+
     return maze
 
 
-maze = BaseOfTheMaze(size,maze)
+maze = BaseOfTheMaze(size, maze)
 
-def IsCaseValidForGenerator(x,y):
-    if 0 <= x < size and 0 <= y < size and maze[x][y] == "." and (x,y) not in generator_stack:
+
+def IsCaseValidForGenerator(x, y):
+    if (
+        0 <= x < size
+        and 0 <= y < size
+        and maze[x][y] == "."
+        and (x, y) not in generator_stack
+    ):
         return True
     return False
 
-def MazeGenerator(x,y):
-    maze[x][y]="."
 
-    random.shuffle(generator_movement)
+def MazeGenerator(x, y):
+    maze[x][y] = "."
 
-    for MoveX , MoveY in generator_movement:
-        NewX = x + MoveX 
+    random.shuffle(MOVES)
+
+    for MoveX, MoveY in MOVES:
+        NewX = x + MoveX
         NewY = y + MoveY
 
-        if IsCaseValidForGenerator(NewX,NewY):
-            generator_stack.append((NewX,NewY))
-            maze[x + MoveX //2][y + MoveY //2] = "."
-            MazeGenerator(NewX,NewY)
+        if IsCaseValidForGenerator(NewX, NewY):
+            generator_stack.append((NewX, NewY))
+            maze[x + MoveX // 2][y + MoveY // 2] = "."
+            MazeGenerator(NewX, NewY)
 
     return maze
 
 
-
 def Printmaze(maze):
-    n=0
+    n = 0
     for i in maze:
         print("".join(maze[n]))
-        n+=1
+        n += 1
+
+
+def MakeFile(name):
+    with open(f"{name}.txt", "w") as file:
+
+        sys.stdout = file
+        print(f"Labyrinthe en backtracking de taille {(size-1 )/2}: ")
+        Printmaze(maze)
+
+        sys.stdout = sys.__stdout__
+
+
+def main_backtracking_generator():
+    print("génération du labyrinthe de taile ", (size - 1) / 2)
+    MazeGenerator(1, 1)
+    Printmaze(maze)
+    MakeFile(name_file)
+    return maze
+
 
 if __name__ == "__main__":
-    print("génération du labyrinthe")
-    MazeGenerator(1,1)
-    Printmaze(maze)
-
+    main_backtracking_generator()
--- maze_solver_ilyes.py
+++ maze_solver_ilyes.py
@@ -1,51 +1,77 @@
-from maze_generator_ilyes import MazeGenerator , size , Printmaze
+from maze_generator_ilyes import (
+    MazeGenerator,
+    size,
+    Printmaze,
+    name_file,
+    main_backtracking_generator,
+)
 import random
+import sys
 
 CHAR_WALL = "#"
 CHAR_WAY = "."
 CHAR_VISITED = "*"
 CHAR_PATH = "o"
-maze_for_solver = MazeGenerator(1,1)
 
-solver_stack=[]
-MOVES = [(-2, 0), (2, 0), (0, -2), (0, 2)]
+maze_for_solver = main_backtracking_generator()
+solver_stack = []
+MOVES = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
 
 def baseOfTheMaze():
     maze_for_solver[0][0] = CHAR_PATH
     maze_for_solver[1][0] = CHAR_PATH
-    maze_for_solver[size-2][size-1] = CHAR_PATH
-    maze_for_solver[size-1][size-1] = CHAR_PATH
+    maze_for_solver[size - 2][size - 1] = CHAR_PATH
+    maze_for_solver[size - 1][size - 1] = CHAR_PATH
+
 
 baseOfTheMaze()
 
-def validCaseSolver(x,y):
+
+def validCaseSolver(x, y):
     if 0 <= x < size and 0 <= y < size and maze_for_solver[x][y] == CHAR_WAY:
         return True
     return False
-    
 
-def mazeSolver(x,y):
-    if x and y == size - 1 and size - 1 :
+
+def mazeSolver(x, y):
+    if x == size - 2 and y == size - 2:
         return True
     else:
-    
-        maze_for_solver[x][y] = CHAR_VISITED
+
+        maze_for_solver[x][y] = CHAR_PATH
 
         random.shuffle(MOVES)
 
-        for dx , dy in MOVES:
+        for dx, dy in MOVES:
             new_x = x + dx
             new_y = y + dy
 
-            if validCaseSolver(x + dx , y + dy) and maze_for_solver[(x + new_x) // 2][(y + new_y) // 2] == CHAR_WAY :
-                maze_for_solver[(x + new_x) // 2][(y + new_y) // 2] = CHAR_VISITED
-                if mazeSolver(new_x,new_y):
-                    maze_for_solver[x][y] = CHAR_PATH
+            if validCaseSolver(new_x, new_y):
+                if mazeSolver(new_x, new_y):
+                    maze_for_solver[new_x][new_y] = CHAR_PATH
+                    return True
+                else:
+                    maze_for_solver[new_x][new_y] = CHAR_VISITED
+
+
+def MakeFile(name):
+    with open(f"{name}.txt", "a") as file:
+
+        sys.stdout = file
+        print(" ")
+        print("resolution en backtracking : ")
+        Printmaze(maze_for_solver)
+
+        sys.stdout = sys.__stdout__
+
+
+def main_backtracking_solver():
+    mazeSolver(1, 1)
+    Printmaze(maze_for_solver)
+    MakeFile(name_file)
 
 
 if __name__ == "__main__":
-    
-    Printmaze(maze_for_solver)
-    mazeSolver(1,1)
-    print("_________________________")
-    Printmaze(maze_for_solver)
+
+    main_backtracking_solver()
